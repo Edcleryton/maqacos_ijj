ad21439438b9c4bc5d33c10d0f32f79c532e34f5
ThaliLaskaski <thalianelaskaski21@gmail.com>
2025-03-26 20:04:17 -0300

    licao 2

---
+++ licao_thali/licao_2.py
@@ -0,0 +1,17 @@
+cliente1 = ("Paula Martins")
+cliente2 = ("João Silva")
+cliente3= ("Thaliane Laskaski")
+
+compra1 = (500.00)
+compra2 = (300.00)
+compra3 = (660.00)
+
+cupom1 = ("PAULA")
+cupom2 = ("JOAO")
+cupom3 = ("THALIANE")
+
+print(f"Olá, {cliente1}. Em JANEIRO você realizou uma compra no valor de {compra1} e ganhou um desconto de 10% em sua próxima compra. Use o cupom {cupom1}É10.")
+
+print(f"Olá, {cliente2}. Em JANEIRO você realizou uma compra no valor de {compra2} e ganhou um desconto de 10% em sua próxima compra. Use o cupom {cupom2}É10.")
+
+print(f"Olá, {cliente3}. Em JANEIRO você realizou uma compra no valor de {compra3} e ganhou um desconto de 10% em sua próxima compra. Use o cupom {cupom3}É10.")
