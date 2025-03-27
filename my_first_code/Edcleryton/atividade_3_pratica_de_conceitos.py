77df139b2a00be345ab921c3381cedecb843f043
Edcleryton Silva <edcleryton.gabriel@gmail.com>
2025-03-27 20:23:59 -0300

    Commit inicial da atividade pratica de conceitos

---
+++ my_first_code/Edcleryton/atividade_3_pratica_de_conceitos.py
@@ -0,0 +1,9 @@
+# Capturando os dados do usuário
+nome = input("Digite seu nome: ")
+altura = float(input("Digite sua altura em metros: "))
+idade = int(input("Digite sua idade: "))
+
+# Exibindo os dados na tela
+print("Nome:", nome)
+print("Altura:", altura, "metros")
+print("Idade:", idade, "anos")
