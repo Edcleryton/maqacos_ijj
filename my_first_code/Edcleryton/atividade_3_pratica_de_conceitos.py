ce1e1f300c6110416fdff55c4b1764d2a4f032b1
Edcleryton Silva <edcleryton.gabriel@gmail.com>
2025-03-27 20:27:48 -0300

    feat: Nova feature: Captura de notas

--- my_first_code/Edcleryton/atividade_3_pratica_de_conceitos.py
+++ my_first_code/Edcleryton/atividade_3_pratica_de_conceitos.py
@@ -3,7 +3,14 @@ nome = input("Digite seu nome: ")
 altura = float(input("Digite sua altura em metros: "))
 idade = int(input("Digite sua idade: "))
 
-# Exibindo os dados na tela
+# Nova feature: Capturando notas
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+soma = nota1 + nota2  # Somando as notas
+
+# Exibindo todos os dados
+print("\n--- Dados do Usuário ---")
 print("Nome:", nome)
 print("Altura:", altura, "metros")
 print("Idade:", idade, "anos")
+print("Soma das notas:", soma)
