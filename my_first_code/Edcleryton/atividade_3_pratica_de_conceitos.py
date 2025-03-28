ce3b6d1ccbdcf9b7ff293c6de0031d3947183f34
Edcleryton Silva <edcleryton.gabriel@gmail.com>
2025-03-27 21:10:30 -0300

    Adicionei validação de tipos com type()

--- my_first_code/Edcleryton/atividade_3_pratica_de_conceitos.py
+++ my_first_code/Edcleryton/atividade_3_pratica_de_conceitos.py
@@ -7,6 +7,17 @@ idade = int(input("Digite sua idade: "))
 nota1 = float(input("Digite a primeira nota: "))
 nota2 = float(input("Digite a segunda nota: "))
 soma = nota1 + nota2  # Somando as notas
+media = (nota1 + nota2) / 2 # Calculando a média
+
+#  identificando o tipo de dado das variavéis
+
+tipo_nome = type(nome)
+tipo_idade = type(idade) 
+tipo_altura = type(altura) 
+tipo_nota1 = type(nota1)
+tipo_nota2 = type(nota2)
+tipo_soma = type(soma)
+tipo_media = type(media)
 
 # Exibindo todos os dados
 print("\n--- Dados do Usuário ---")
@@ -14,3 +25,13 @@ print("Nome:", nome)
 print("Altura:", altura, "metros")
 print("Idade:", idade, "anos")
 print("Soma das notas:", soma)
+print("Média das notas:", media)
+
+# Tipo dos dados
+print("Tipo de dado de 'nome':", tipo_nome) 
+print("Tipo de dado de 'idade':", tipo_idade) 
+print("Tipo de dado de 'altura':", tipo_altura)
+print("Tipo de dado de 'nome':", tipo_nota1)
+print("Tipo de dado de 'nome':", tipo_nota2)
+print("Tipo de dado de 'nome':", tipo_soma)
+print("Tipo de dado de 'nome':", tipo_media)
