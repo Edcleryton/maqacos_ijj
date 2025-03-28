330c0027ab831e1524e403cc79ae07e2aef115b9
Danilo Melin <dmelin88@gmail.com>
2025-03-27 21:36:48 -0300

    Executando atividade A3M4_3

---
+++ danilo/atividade_A3M4_3.py
@@ -0,0 +1,6 @@
+nome = input("Digite seu nome: ")
+altura = float(input("Digite sua altura em metros (ex: 1.76)"))
+idade = int(input("Digite sua idade: "))
+
+print(f"Nome do usuário: {nome}, sua altura é {altura}, sua idade é: {idade}")
+
