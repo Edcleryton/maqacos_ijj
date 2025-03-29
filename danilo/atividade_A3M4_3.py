d79325cb63d7dc2168e3253d32379fface2c30f1
Danilo Melin <dmelin88@gmail.com>
2025-03-29 15:13:46 -0300

    feat: adicionada a funcionalidade de notas

--- danilo/atividade_A3M4_3.py
+++ danilo/atividade_A3M4_3.py
@@ -1,6 +1,14 @@
+# Faça um programa que capture o nome do usuário, altura em metros, idade e imprima esses dados na tela.
 nome = input("Digite seu nome: ")
 altura = float(input("Digite sua altura em metros (ex: 1.76)"))
 idade = int(input("Digite sua idade: "))
 
 print(f"Nome do usuário: {nome}, sua altura é {altura}, sua idade é: {idade}")
 
+# Agora, implemente uma nova Feature: a funcionalidade de notas.
+nota1 = float(input("Insira sua primeira nota: "))
+nota2 = float(input("Insira sua segunda nota: "))
+
+media_notas = (nota1 + nota2) / 2
+
+print(f"Sua media final foi de {media_notas}")
