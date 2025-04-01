94500d6e95afbd65300d8d6064fef4b485dd2729
ThaliLaskaski <thalianelaskaski21@gmail.com>
2025-04-01 19:34:48 -0300

    lição 4

---
+++ licao_thali/licao_4.py
@@ -0,0 +1,30 @@
+# Capturando os dados do usuário
+
+nome = input("Digite seu nome: ")
+altura = input("Digite sua altura em metros: ").replace(",", ".")
+altura = float(altura)
+idade = int(input("Digite sua idade: "))
+
+# Nova feature: Capturando notas
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+soma = nota1 + nota2  
+media = (nota1 + nota2) / 2
+
+print("\n--- Dados do Usuário ---")
+print(f"Nome: {nome}")
+print(f"Altura: {altura:.2f} metros")
+print(f"Idade: {idade} anos")
+print(f"Soma das notas: {soma:.2f}")
+print(f"Média das notas: {media:.2f}")
+
+# Capturando os tipos de dados
+tipo_nome = type(nome)
+tipo_idade = type(idade)
+tipo_altura = type(altura)
+
+# Exibindo os tipos de dados
+print("Tipo de dado de 'nome':", tipo_nome)
+print("Tipo de dado de 'idade':", tipo_idade)
+print("Tipo de dado de 'altura':", tipo_altura)
+
