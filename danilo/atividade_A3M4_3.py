3ed6eb0bf3700ccd376b9a1e737b07d9e9263fd9
Danilo Melin <dmelin88@gmail.com>
2025-04-01 20:26:38 -0300

    feat: identificado os tipos de cada variável

--- danilo/atividade_A3M4_3.py
+++ danilo/atividade_A3M4_3.py
@@ -1,6 +1,6 @@
 # Faça um programa que capture o nome do usuário, altura em metros, idade e imprima esses dados na tela.
 nome = input("Digite seu nome: ")
-altura = float(input("Digite sua altura em metros (ex: 1.76)"))
+altura = float(input("Digite sua altura em metros (ex: 1.76): "))
 idade = int(input("Digite sua idade: "))
 
 print(f"Nome do usuário: {nome}, sua altura é {altura}, sua idade é: {idade}")
@@ -12,3 +12,7 @@ nota2 = float(input("Insira sua segunda nota: "))
 media_notas = (nota1 + nota2) / 2
 
 print(f"Sua media final foi de {media_notas}")
+
+print(f"Tipo de váriavel media_notas recebe: {type(media_notas)}")
+print(f"Tipo de váriavel nome recebe: {type(nome)}")
+print(f"Tipo de váriavel idade recebe: {type(idade)}")
