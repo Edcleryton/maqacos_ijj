582e6800f9df284e3acca07b69a20a8a8c756a77
Danilo Melin <dmelin88@gmail.com>
2025-04-02 10:44:41 -0300

    feat: inserido um input para entrada do nome

--- danilo/mini_case_2.py
+++ danilo/mini_case_2.py
@@ -1,4 +1,6 @@
 # Fazer um programa que some 4 notas e, no final, tenha a média aritmétrica dessas notas.
+nome = input("Digite o seu nome: ")
+
 nota1 = float(input("Digite sua 1ª nota: "))
 nota2 = float(input("Digite sua 2ª nota: "))
 nota3 = float(input("Digite sua 3ª nota: "))
@@ -7,4 +9,4 @@ nota4 = float(input("Digite sua 4ª nota: "))
 # Calculo da média das notas
 media = (nota1 + nota2 + nota3 + nota4) / 4
 
-print(f"Sua media final é: {media}")
+print(f"Olá, {nome}! Sua média final é: {media}")
