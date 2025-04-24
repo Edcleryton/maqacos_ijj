9dedc75ed5b423cca5ce21ad92c0f7dca322d3ad
Edcleryton Silva <edcleryton.gabriel@gmail.com>
2025-04-24 14:09:55 -0300

    ✨ feat: Adição da primeira versão do mini case  atividade 4

--- edcleryton/atividade_4.py
+++ edcleryton/atividade_4.py
@@ -1,7 +1,10 @@
-x= (5+3)*2
-y= 25e9
-print(x, y)
-resultado = 10 / 2 + 3 * 5
-resultado2 = 2 * 3 ** 2
-print(resultado)
-print(resultado2)
+#Mini Case 1: Idade do Pet e Lucro do PETSHOP
+print("Olá, seja bem viendo a nossa loja!")
+print("Para melhor atende-lo(a), precisamos de algumas informações.")
+print("Vamos lá?")
+nome_do_tutor = input("Digite o nome do tutor: ")
+nome_do_pet = input("Digite o nome do pet: ")
+idade = int(input("Agora digite a idade do cachorro: "))
+idade_cahorro = idade * 7
+
+print(f"Muito obrigado {nome_do_tutor}! A idade do(a) {nome_do_pet} é {idade_cahorro} anos.")
